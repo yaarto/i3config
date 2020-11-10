87ae0fb3134f897a4532b5ba45bfe958b45ffa47
Hoang-Anh Pham <60178934+yaarto@users.noreply.github.com>
2020-11-10 14:12:57 +0700

    Update i3lock.sh

--- bin/i3lock.sh
+++ bin/i3lock.sh
@@ -1,11 +1,10 @@
 #!/bin/sh
-scrot /tmp/screen_locked.png
-convert /tmp/screen_locked.png -blur 2x2  -charcoal 1 -colorize 70%   /tmp/screen_locked.png
-convert /tmp/screen_locked.png -blur 1x1  -implode 1 -colorize 20%   /tmp/screen_locked.png
-#convert /tmp/screen_locked.png -pointsize 50 -fill white -draw 'text 300,200 "TxGVNN"' /tmp/screen_locked.png
-convert /tmp/screen_locked.png -scale 20% -scale 500% /tmp/screen_locked.png
-convert /tmp/screen_locked.png ~/.i3/bin/lock.png -gravity center -composite -matte /tmp/screen_locked.png
-if ! i3lock -i /tmp/screen_locked.png; then
+IMG=Pictures/Wallpaper/Wallpaper.png  
+convert $IMG -blur 2x2  -charcoal 1 -colorize 70%  $IMG
+convert $IMG -blur 1x1  -implode 1 -colorize 20%   $IMG
+convert $IMG -scale 50% -scale 200% $IMG
+convert $IMG ~/.config/i3/bin/lock.png -gravity center -composite -matte $IMG
+if ! i3lock -i $IMG; then
     return 1
 fi
 xset dpms force off
